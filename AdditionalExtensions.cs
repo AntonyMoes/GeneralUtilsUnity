bb09134dcdeb9e99a18204a67767c1da7e07a865
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2024-03-12 20:26:49 +0400

    Add core meta and fix extensions

---
+++ AdditionalExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace GeneralUtils {
+    public static class AdditionalExtensions {
+        #region Color
+
+        public static Color WithAlpha(this Color color, float alpha) {
+            var result = color;
+            result.a = alpha;
+            return result;
+        }
+
+        #endregion
+
+        #region Vector3
+
+        public static Vector3 With(this Vector3 vector, float? x = null, float? y = null, float? z = null) {
+            var newVector = vector;
+
+            if (x is { } newX) {
+                newVector.x = newX;
+            }
+
+            if (y is { } newY) {
+                newVector.y = newY;
+            }
+
+            if (z is { } newZ) {
+                newVector.z = newZ;
+            }
+
+            return newVector;
+        }
+
+        #endregion
+
+        #region Vector2
+
+        public static Vector2 With(this Vector2 vector, float? x = null, float? y = null) {
+            var newVector = vector;
+
+            if (x is { } newX) {
+                newVector.x = newX;
+            }
+
+            if (y is { } newY) {
+                newVector.y = newY;
+            }
+
+            return newVector;
+        }
+
+        #endregion
+    }
+}
